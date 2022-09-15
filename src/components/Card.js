94b6f0a99cfa827e9e32aed370c2cc21b1a67625
Cindy Lam <cindybinh444@gmail.com>
2022-09-15 00:07:44 -0400

    (REUSABLE COMPONENT) Added Card

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import Button from "./Button";
+import "./Card.css";
+
+export default function Card({
+  keyId,
+  name,
+  price,
+  time,
+  description,
+  handleCardButtonClick,
+}) {
+  return (
+    <div className="cardCollection__div_lg" key={keyId}>
+      <div className="cardHeader__div_lg">
+        <h3>{name}</h3>
+        <Button text="Add+" handleClick={handleCardButtonClick} customStyling="cardButton__btn_lg"/>
+      </div>
+      <div className="cardTimePrice__div_lg">
+        <span>{price}</span>
+        <span>{time} hours</span>
+      </div>
+      <div>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
